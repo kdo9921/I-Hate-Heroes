74fea7a7933339dc3c280643f160a01cf1fd496e
키위 <kdo9921@gmail.com>
2018-03-25 20:49:10 +0900

    커스텀 쉽게
    이제 변수로 쉽게 시공의 갯수를 늘릴수 있습니다

--- I Hate Heroes.py
+++ I Hate Heroes.py
@@ -10,7 +10,8 @@ Tk().wm_withdraw()
 SiGongJoA = pygame.image.load('heroes.png')
 pygame.display.set_icon(SiGongJoA)
 
-width, height = 400, 600
+width, height = 400, 600    #화면크기 설정
+howManySigong = 3   #난이도 설정 (낮을수록 쉽고 높을수록 어렵습니다)
 
 class Heroes:
     heroes = pygame.image.load('heroes.png')
@@ -33,18 +34,14 @@ def playGame():
     global monitor, person, heroes
     personX = width/2 - (personSize[0]/2)
     dx = 0
+    hms = howManySigong
 
-    h1 = Heroes()
-    h2 = Heroes()
-    h3 = Heroes()
+    SG = [0] * hms
 
-    h1.heroesX = random.randrange(0,width-70)
-    h2.heroesX = random.randrange(0,width-70)
-    h3.heroesX = random.randrange(0,width-70)
-
-    h1.heroesSpeed = random.randrange(5,10)
-    h2.heroesSpeed = random.randrange(7,13)
-    h3.heroesSpeed = random.randrange(10,15)
+    for i in range(0,hms):
+        SG[i] = Heroes()
+        SG[i].heroesX = random.randrange(0,width-70)
+        SG[i].heroesSpeed = random.randrange(5,15)
 
     while True:
         (pygame.time.Clock()).tick(100)
@@ -70,40 +67,20 @@ def playGame():
 
         paintEntity(person, personX, height-81)
 
-        h1.heroesY += h1.heroesSpeed
-        h2.heroesY += h2.heroesSpeed
-        h3.heroesY += h3.heroesSpeed
+        for i in range(0, hms):
+            SG[i].heroesY += SG[i].heroesSpeed
 
-        if h1.heroesY > height:
-            h1.heroesY = -h1.heroesSize[0]
-            h1.heroesX = random.randrange(0,width-h1.heroesSize[0])
-            h1.heroesSpeed = GetHeroesSpeed()
+        for i in range(0, hms):
+            if SG[i].heroesY > height:
+                SG[i].heroesY = -SG[i].heroesSize[0]
+                SG[i].heroesX = random.randrange(0,width-SG[i].heroesSize[0])
+                SG[i].heroesSpeed = GetHeroesSpeed()
 
-        if h2.heroesY > height:
-            h2.heroesY = -h2.heroesSize[0]
-            h2.heroesX = random.randrange(0,width-h2.heroesSize[0])
-            h2.heroesSpeed = GetHeroesSpeed()
-        
-        if h3.heroesY > height:
-            h3.heroesY = -h3.heroesSize[0]
-            h3.heroesX = random.randrange(0,width-h3.heroesSize[0])
-            h3.heroesSpeed = GetHeroesSpeed()
-
-        paintEntity(h1.heroes, h1.heroesX, h1.heroesY)
-        paintEntity(h2.heroes, h2.heroesX, h2.heroesY)
-        paintEntity(h3.heroes, h3.heroesX, h3.heroesY)
-
-        if (height - 120 < h1.heroesY < height):
-            if (personX < h1.heroesX + 55) and (h1.heroesX < personX + 25):
-                gameover()
-
-        if (height - 120 < h2.heroesY < height):
-            if (personX < h2.heroesX + 55) and (h2.heroesX < personX + 25):
-                gameover()
-
-        if (height - 120 < h3.heroesY < height):
-            if (personX < h3.heroesX + 55) and (h3.heroesX < personX + 25):
-                gameover()
+        for i in range(0, hms):
+            paintEntity(SG[i].heroes, SG[i].heroesX, SG[i].heroesY)
+            if (height - 120 < SG[i].heroesY < height):
+                if (personX < SG[i].heroesX + 55) and (SG[i].heroesX < personX + 25):
+                    gameover()
 
         pygame.display.update()
 
